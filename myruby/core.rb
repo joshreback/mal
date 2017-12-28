ac775270d8a67f0becd8d12ff61d88947df1b5f9
Josh Reback <jreback@careersofia.com>
2017-12-27 22:11:38 -0800

    step 4 done

--- myruby/core.rb
+++ myruby/core.rb
@@ -10,7 +10,7 @@ NS = {
   'list?': lambda { |mal_type| MalAtom.new((mal_type.type == "MalList").to_s) },
   'empty?': lambda { |mal_type| MalAtom.new((mal_type.length == 0).to_s) },
   'count': lambda { |mal_list| MalNum.new(mal_list.type == "MalList" ? mal_list.length : 0) },
-  '=': lambda { |a, b| MalAtom.new(a.mal_eq(b)) },
+  '=': lambda { |a, b| MalAtom.new(a.mal_eq(b).to_s) },
   '<': lambda { |a, b| a < b },
   '<=': lambda { |a, b| a <= b },
   '>': lambda { |a, b| a > b },
